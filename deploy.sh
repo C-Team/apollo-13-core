3a111eab455dbc546d16552f8c7d42dd7a1e6530
Brendan Higgins <bhig93@gmail.com>
2015-05-15 19:00:52 -0400

    Added start up script.

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,4 @@
 scp apollo/core/hello root@192.168.7.2:~
 scp apollo/remote/remote root@192.168.7.2:~
 scp apollo/control/control root@192.168.7.2:~
+scp start_up.sh root@192.168.7.2:~
---
+++ start_up.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+
+# Enable ADC device files.
+echo cape-bone-iio > /sys/devices/bone_capemgr.9/slots
